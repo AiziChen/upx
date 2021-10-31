1779f17059ecae82a7cdbac3baf25afc0614d256
aizichen <1098679804@qq.com>
2021-10-31 14:42:08 +0800

    remove some upx information

--- src/pefile.cpp
+++ src/pefile.cpp
@@ -2568,8 +2568,8 @@ void PeFile::pack0(OutputFile *fo, ht &ih, ht &oh,
     const unsigned ncsize_virt_increase = (ncsize & oam1) == 0 ? 8 : 0;
 
     // fill the sections
-    strcpy(osection[0].name,"UPX0");
-    strcpy(osection[1].name,"UPX1");
+    strcpy(osection[0].name,"0x07FA8");
+    strcpy(osection[1].name,"0x8BEF1");
     // after some windoze debugging I found that the name of the sections
     // DOES matter :( .rsrc is used by oleaut32.dll (TYPELIBS)
     // and because of this lame dll, the resource stuff must be the
@@ -2578,7 +2578,7 @@ void PeFile::pack0(OutputFile *fo, ht &ih, ht &oh,
     // ... even worse: exploder.exe in NiceTry also depends on this to
     // locate version info
 
-    strcpy(osection[2].name, !last_section_rsrc_only && soresources ? ".rsrc" : "UPX2");
+    strcpy(osection[2].name, !last_section_rsrc_only && soresources ? ".rsrc" : "0010110");
 
     osection[0].vaddr = rvamin;
     osection[1].vaddr = s1addr;
--- src/stub/src/include/linux.h
+++ src/stub/src/include/linux.h
@@ -685,13 +685,13 @@ typedef struct
 // !!! must be the same as in p_unix.h !!!
 #define OVERHEAD        2048
 
-#define UPX_MAGIC_LE32  0x21585055          // "UPX!"
+#define UPX_MAGIC_LE32  0xB7A834F          // "UPX!"
 
 #if 1
 // patch constants for our loader (le32 format)
 //#define UPX1            0x31585055          // "UPX1"
 #define UPX2            0x32585055          // "UPX2"
-#define UPX3            0x33585055          // "UPX4"
+#define UPX3            0x33585055          // "UPX3"
 #define UPX4            0x34585055          // "UPX4"
 //#define UPX5            0x35585055          // "UPX5"
 #else
